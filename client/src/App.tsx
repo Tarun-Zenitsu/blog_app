0992e81a9623013c9fe797f63bbaaee448d22f99
Tarun-Zenitsu <btkkumbhar1998@gmail.com>
2024-01-19 19:14:03 +0530

    Header completed

---
+++ client/src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/sign-in' element={<SignIn />}/>
+        <Route path='/sign-up' element={<SignUp />}/>
+        <Route path='/dashboard' element={<Dashboard />}/>
+        <Route path='/projects' element={<Projects />}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
